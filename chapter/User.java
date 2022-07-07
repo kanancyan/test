833e52b0d00ab39c82902f274f9a7938e9034d3f
kannacyan <3331279769@qq.com>
2022-07-08 01:01:30 +0800

    first commit

---
+++ chapter/User.java
@@ -0,0 +1,31 @@
+package chapter;
+
+public class User {
+
+	private String ID;
+	private String commodity;
+	private double price;
+	private String kinds;
+	private String userName;
+	private String local;
+	
+	public void addCommodity() {
+		return;
+	}
+	public void addUers()
+	{
+		return;
+	}
+	public void findsGoods()
+	{
+		return;
+	}
+	public void buy()
+	{
+		return;
+	}
+	public void getUersInfo()
+	{
+		System.out.println("用户信息："+ID+userName+local);
+	}
+}
